cd04c4db8a2c7e1b9d10dcb0b5ca740b385d755e
csioa <babispasg@gmail.com>
2017-08-09 23:50:52 +0100

    Split to functions, added creation of plot

--- wcBooks.py
+++ wcBooks.py
@@ -1,29 +1,62 @@
 import yaml
+import pandas as pd
+import seaborn as sns
+import matplotlib
+matplotlib.style.use('ggplot')
+import matplotlib.pyplot as plt
 from tabulate import tabulate
 from pyspark import SparkContext
-from pyspark.sql import SQLContext
 
-with open("books.yaml","r") as booksList:
-    books = yaml.load(booksList)["information"]
+FILENAME = "books.yaml"
 
-books = map(lambda book: book["path"], books)
+KEY_WORDS = ["love", "war", "peace","freedom", "soul"
+             "man", "woman","kill", "luck", "destiny"]
 
-sc = SparkContext("local[4]", "BookWordCount")
-sqlContext = SQLContext(sc)
 
-wordList = sc.broadcast(["love", "war", "peace", "hate",
-                         "man", "woman", "he", "she", "slave",
-                         "freedom", "kill", "luck","him", "her"])
-booksRDD = sc.textFile(','.join(books))
+def plot_rankings(ranking):
+    df = pd.DataFrame(ranking.values(), index=ranking.keys())
 
-ranking = (booksRDD.flatMap(lambda line: line.split(" "))
-                    .map(lambda word: word.lower())
-                    .filter(lambda word: word in wordList.value)
-                    .map(lambda word: (word, 1))
-                    .reduceByKey(lambda a, b: a + b)
-                    .collect())
+    plt.figure()
 
-ranking = dict(ranking)
-sorted_rankings = sorted(ranking.items(), key=lambda (k,v): v, reverse=True)
+    ax = df.sort_values(0).plot.barh(legend=False)
+    fig = ax.get_figure()
+    fig.savefig('word_ranking.png')
 
-print tabulate(sorted_rankings, headers=["Word", "Occurences"])
+
+def get_books(filename):
+    with open(filename, "r") as booksList:
+        books = yaml.load(booksList)["information"]
+
+    books = map(lambda book: book["path"], books)
+    return books
+
+
+def spark_wordcount():
+    sc = SparkContext("local[4]", "BookWordCount")
+
+    wordList = sc.broadcast(KEY_WORDS)
+    booksRDD = sc.textFile(','.join(get_books(FILENAME)))
+
+    ranking = (booksRDD.flatMap(lambda line: line.split(" "))
+               .map(lambda word: word.lower())
+               .filter(lambda word: word in wordList.value)
+               .map(lambda word: (word, 1))
+               .reduceByKey(lambda a, b: a + b)
+               .collect())
+    return ranking
+
+
+def main():
+
+    ranking = spark_wordcount()
+
+    ranking = dict(ranking)
+    sorted_rankings = sorted(ranking.items(), key=lambda (k, v): v, reverse=True)
+
+    print tabulate(sorted_rankings, headers=["Word", "Occurrences"])
+
+    plot_rankings(ranking)
+
+if __name__=="__main__":
+    main()
+
